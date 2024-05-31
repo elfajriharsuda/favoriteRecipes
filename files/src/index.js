ed4949b41ce3714ce1bca6707b2d004673b617e7
elfajriharsuda <elfajri.harsuda@gmail.com>
2024-06-01 01:51:58 +0700

    add project Favorite Recipe

---
+++ files/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./app/App.js";
+import { store } from "./app/store.js";
+
+const root = createRoot(document.getElementById("root"));
+const render = () => {
+  root.render(<App state={store.getState()} dispatch={store.dispatch} />);
+};
+store.subscribe(render);
+render();
